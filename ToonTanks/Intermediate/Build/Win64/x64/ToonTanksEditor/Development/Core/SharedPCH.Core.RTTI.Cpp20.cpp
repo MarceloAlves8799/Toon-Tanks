cdb8f2794b0ef5033c4f1f4dde7e18b206ac5a21
Marcelo Alves <48249122+MarceloAlves8799@users.noreply.github.com>
2023-10-31 20:53:03 -0300

    Initial
    1) Initial setup for project, create base C++ script for BasePawn to be inheritance for Tank and Turret Pawn, learn about forwarding declaration, and how to declare property read/edit variables field in Instance and Default

---
+++ ToonTanks/Intermediate/Build/Win64/x64/ToonTanksEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "D:/Unreal/Toon-Tanks/ToonTanks/Intermediate/Build/Win64/x64/ToonTanksEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
---
+++ ToonTanks/Intermediate/Build/Win64/x64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "D:/Unreal/Toon-Tanks/ToonTanks/Intermediate/Build/Win64/x64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h"
